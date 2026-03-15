994b21fdd22ae3e92ec599ae89dcee5db50069a1
Маша <solodomasha@gmail.com>
2026-03-15 22:08:18 +0300

    ...

--- taxi5/ClientMenu.cs
+++ taxi5/ClientMenu.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Npgsql;
+using System;
 using System.Windows.Forms;
 
 namespace taxi4
@@ -13,11 +7,106 @@ namespace taxi4
     public partial class ClientMenu : Form
     {
         public string Role { get; set; }
-        public int AccountId { get; set; }
+        public int AccountId { get; private set; }
         public string UserLogin { get; set; }
+        public int ClientId { get; private set; }
+
+        // Конструктор с параметром accountId
+        public ClientMenu(int accountId)
+        {
+            InitializeComponent();
+            AccountId = accountId;
+            LoadClientId();
+        }
+
+        // Для совместимости с дизайнером (не удалять)
         public ClientMenu()
         {
             InitializeComponent();
+        }
+
+        private void LoadClientId()
+        {
+            string connectionString = "Server=localhost;Port=5432;Database=taxi4;User Id=postgres;Password=123";
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT client_id FROM client WHERE account_id = @accountId";
+                using (var cmd = new NpgsqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@accountId", AccountId);
+                    var result = cmd.ExecuteScalar();
+                    if (result != null)
+                        ClientId = Convert.ToInt32(result);
+                }
+            }
+        }
+
+        private void btnNewOrder_Click(object sender, EventArgs e)
+        {
+            ClientOrderForm orderForm = new ClientOrderForm(this.AccountId);
+            orderForm.Closed += (s, args) => this.Show();
+            orderForm.Show();
+            this.Hide();
+        }
+
+        private void btnTrackOrder_Click(object sender, EventArgs e)
+        {
+            if (ClientId == 0)
+            {
+                MessageBox.Show("Не удалось определить ID клиента", "Ошибка");
+                return;
+            }
+            ClientTrackOrderForm trackForm = new ClientTrackOrderForm(this.ClientId);
+            trackForm.Closed += (s, args) => this.Show();
+            trackForm.Show();
+            this.Hide();
+        }
+
+        private void btnHistory_Click(object sender, EventArgs e)
+        {
+            if (ClientId == 0)
+            {
+                MessageBox.Show("Не удалось определить ID клиента", "Ошибка");
+                return;
+            }
+            ClientOrderHistoryForm historyForm = new ClientOrderHistoryForm(this.ClientId);
+            historyForm.Closed += (s, args) => this.Show();
+            historyForm.Show();
+            this.Hide();
+        }
+
+        private void btnMyAddresses_Click(object sender, EventArgs e)
+        {
+            if (ClientId == 0)
+            {
+                MessageBox.Show("Не удалось определить ID клиента", "Ошибка");
+                return;
+            }
+            ClientPointForm pointForm = new ClientPointForm(this.ClientId);
+            pointForm.Closed += (s, args) => this.Show();
+            pointForm.Show();
+            this.Hide();
+        }
+
+        private void btnPromotions_Click(object sender, EventArgs e)
+        {
+            if (ClientId == 0)
+            {
+                MessageBox.Show("Не удалось определить ID клиента", "Ошибка");
+                return;
+            }
+            ClientPromotionForm promoForm = new ClientPromotionForm(this.ClientId);
+            promoForm.Closed += (s, args) => this.Show();
+            promoForm.Show();
+            this.Hide();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Hide();
         }
     }
 }
